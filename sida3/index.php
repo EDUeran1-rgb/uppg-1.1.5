28432192fc246c475aacdaa373d7668f74fef881
EDUeran1-rgb <EDUeran1@utb.karlskoga.se>
2025-10-14 13:25:20 +0200

    hmm

---
+++ sida3/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$dbname="databas1";
+$conn=mysqli_connect($host,$user,$pass,$dbname);
+
+$texten="";
+if(isset($post['btn'])){
+    $texten=$_POST['txt'];
+    $sql="INSERT INTO namn(url) VALUES ('$texten')";
+    $result=mysqli_query($conn,$sql);
+}
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="txt" placeholder="Ange en länk">
+        <input type="submit" value="lagra länk" name="btn">
+    </form>
+    <?php
+    $sql="SELECT * FROM namn";
+    $result=mysqli_query($conn,$sql);
+    while($row=mysqli_fetch_assoc($result)){
+        echo "<h1>".$row['url']."</h1>";
+    }
+    ?>
+</body>
+</html>
